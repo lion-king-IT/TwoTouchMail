0be1d8526cd443c91158b577a102a41d89ae0636
github lion-king-IT <reo.runningg@gmail.com>
2020-10-30 12:25:53 +0900

    Initial commit

---
+++ app/src/main/java/reo/running/twotouchmail/MainActivity.java
@@ -0,0 +1,36 @@
+package reo.running.twotouchmail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnPickUp = (Button)this.findViewById(R.id.button1);
+        btnPickUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,PickUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnNoDinner = (Button)this.findViewById(R.id.button2);
+        btnNoDinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,NoDinnerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
